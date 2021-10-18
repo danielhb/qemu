78c8e5cf3c0f460524c50efc710b5aea12932558
Daniel Henrique Barboza <danielhb413@gmail.com>
2021-10-17 22:01:23 -0300

    target/ppc: introduce PMU events
    This patch starts an IBM Power8+ compatible PMU implementation by adding
    the representation of PMU events that we are going to sample, PMUEvent.
    This struct represents a Perf event, determined by the PMUEventType
    enum, that is being sampled by a specific counter 'sprn'. PMUEvent also
    contains an overflow timer that will be used to trigger cycle overflows
    when cycle events are being sampled. This timer will call
    cpu_ppc_pmu_timer_cb(), which in turn calls fire_PMC_interrupt(). Both
    functions are stubs that will be implemented later on when EBB support
    is added.
    
    The PMU has 6 PMUEvents all the time, one for each counter. Events that
    aren't available (i.e. the counter isn't running) will be of type
    'PMU_EVENT_INVALID'. Other types added in this patch are
    PMU_EVENT_CYCLES and PMU_EVENT_INSTRUCTIONS. More types will be added
    later on.
    
    Two new helper files are created to host this new logic.
    cpu_ppc_pmu_init() will init all PMUEvents during CPU init time.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -296,6 +296,26 @@ typedef struct ppc_v3_pate_t {
     uint64_t dw1;
 } ppc_v3_pate_t;
 
+/* PMU related structs and defines */
+#define PMU_EVENTS_NUM 6
+typedef enum {
+    PMU_EVENT_INVALID = 0,
+    PMU_EVENT_CYCLES,
+    PMU_EVENT_INSTRUCTIONS,
+} PMUEventType;
+
+typedef struct PMUEvent {
+    int sprn;
+    PMUEventType type;
+
+    /*
+     * Timer used to fire performance monitor alerts
+     * when counting cycles.
+     */
+    QEMUTimer *cyc_overflow_timer;
+
+} PMUEvent;
+
 /*****************************************************************************/
 /* Machine state register bits definition                                    */
 #define MSR_SF   63 /* Sixty-four-bit mode                            hflags */
@@ -1191,6 +1211,8 @@ struct CPUPPCState {
     uint32_t tm_vscr;
     uint64_t tm_dscr;
     uint64_t tm_tar;
+
+    PMUEvent pmu_events[PMU_EVENTS_NUM];
 };
 
 #define SET_FIT_PERIOD(a_, b_, c_, d_)          \
